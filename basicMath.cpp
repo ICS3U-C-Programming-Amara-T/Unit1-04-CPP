de3f40a5a84b0aecd8496df9455dca7d083348fa
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-18 10:37:54 +0000

    How to do basic math in CPP

---
+++ basicMath.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Amara Tie All rights reserved.
+
+//
+
+// Created by: Amara Tie
+
+// Date: February 14 2025
+
+// Shows how to do basic math in CPP
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" <<(9+2) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+4/2=" << (4+4/2) << std::endl;
+    std::cout << "3+4*2=" << (3+4*2) << std::endl;
+    std::cout << "5+ pow(2,3)=" << (5+ pow(2,3)) << std::endl;
+
+    return 0;
+}
